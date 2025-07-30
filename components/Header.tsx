07c43ae0f8b70c01793ce1bf04ca0cbdae81762a
themaltezers <themaltezers@gmail.com>
2025-07-30 14:06:20 +0200

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="image">logo</div>
+            <div className="">Menu</div>
+        </header>
+    );
+};
+
+export default Header;
