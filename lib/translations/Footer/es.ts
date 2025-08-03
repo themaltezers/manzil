f939c2db33360b39a5d001b1e00674b0c9a02d4f
themaltezers <themaltezers@gmail.com>
2025-08-03 14:09:47 +0200

    footer traduit

---
+++ lib/translations/Footer/es.ts
@@ -0,0 +1,12 @@
+const es = {
+    contactTitle: "Contáctanos",
+    exploreTitle: "Explora Nuestro Sitio",
+    links: {
+        about: "Acerca de",
+        menu: "Menú",
+        career: "Carrera",
+    },
+    copyright: "© Copyright 2025 Manzil Brunch | Por",
+};
+
+export default es;
---
+++ lib/translations/Footer/it.ts
@@ -0,0 +1,12 @@
+const it = {
+    contactTitle: "Contattaci",
+    exploreTitle: "Esplora il Nostro Sito",
+    links: {
+        about: "Chi siamo",
+        menu: "Menu",
+        career: "Carriera",
+    },
+    copyright: "© Copyright 2025 Manzil Brunch | Di",
+};
+
+export default it;
